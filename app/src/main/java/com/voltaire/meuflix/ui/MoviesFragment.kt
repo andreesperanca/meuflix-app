1b2d2765db33f5c55f4ac0fc576761ca8a38ffe2
andreeesperanca <andreluizesperancacorreia@gmail.com>
2023-01-17 15:04:01 -0300

    change movies fragment UI not refactored

--- app/src/main/java/com/voltaire/meuflix/ui/MoviesFragment.kt
+++ app/src/main/java/com/voltaire/meuflix/ui/MoviesFragment.kt
@@ -1,27 +1,40 @@
 package com.voltaire.meuflix.ui
 
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSmoothScroller
+import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.jackandphantom.carouselrecyclerview.CarouselRecyclerview
-import com.voltaire.meuflix.R
+import androidx.recyclerview.widget.RecyclerView.HORIZONTAL
+import androidx.recyclerview.widget.RecyclerView.VERTICAL
 import com.voltaire.meuflix.adapters.GenreAdapter
 import com.voltaire.meuflix.adapters.HighlightsMoviesAdapter
 import com.voltaire.meuflix.databinding.FragmentMoviesBinding
-import kotlin.math.floor
+import com.voltaire.meuflix.utils.CarouselLayoutManager
+import com.voltaire.meuflix.utils.generics.AutoScrollRecyclerView
+import kotlinx.coroutines.*
 
 class MoviesFragment : Fragment() {
+
+    private val adapterTest by lazy {
+        HighlightsMoviesAdapter()
+    }
 
     val binding by lazy {
         FragmentMoviesBinding.inflate(layoutInflater)
     }
 
-    private lateinit var recyclerViewCarousel: CarouselRecyclerview
+    private lateinit var recyclerViewCarousel: AutoScrollRecyclerView
     lateinit var recyclerView: RecyclerView
+    var position = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,46 +42,81 @@ class MoviesFragment : Fragment() {
         //ConfigureRecyclerViewCarousel
         recyclerViewCarousel = binding.rvMoviesHighlights
         recyclerViewCarousel.apply {
-            adapter = HighlightsMoviesAdapter()
-            set3DItem(true)
-            setInfinite(true)
-            setIntervalRatio(100f)
+
+
+            setCanTouch(true)
+            isLoopEnabled = true
+            setCanTouch(true)
+            startAutoScroll()
+
+
+            val carousel = CarouselLayoutManager(
+                isLoop = false,
+                isItem3D = true,
+                ratio = 1000f,
+                flat = true,
+                alpha = false,
+                isScrollingEnabled = true
+            )
+            val linear = LinearLayoutManager(requireContext(), HORIZONTAL, false)
+
+            layoutManager = linear
+
+            adapter = adapterTest
         }
 
         //ConfigureRecyclerViewMovies
         recyclerView = binding.rvMovies
         recyclerView.apply {
-            layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            layoutManager = LinearLayoutManager(requireContext(), VERTICAL, false)
             adapter = GenreAdapter()
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        setupToolbar()
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        val snapHelper = PagerSnapHelper()
+        snapHelper.attachToRecyclerView(recyclerViewCarousel)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            autoScroll()
+        }
+    }
 
-    fun setupToolbar() {
-        binding.tbMoviesToolbar.apply {
-            inflateMenu(R.menu.main_menu)
-            title = getString(R.string.app_name)
-            setOnMenuItemClickListener {
-                when(it.itemId) {
-                    R.id.fetchFavorites -> {
-                        true
-                    }
-                    R.id.fetchForGenre -> {
-                        true
-                    }
-                    R.id.searchForName -> {
-                        true
-                    }
-                    else -> {false}
-                }
+    @RequiresApi(Build.VERSION_CODES.P)
+    fun autoScroll() {
+        val speedScroll = 3000
+        val handler = Handler()
+        val runnable: java.lang.Runnable = object : java.lang.Runnable {
+            override fun run() {
+                TODO("Not yet implemented")
             }
         }
     }
 }
+
+//    fun setupToolbar() {
+//        binding.tbMoviesToolbar.toolbar.apply {
+//            inflateMenu(R.menu.main_menu)
+//            setOnMenuItemClickListener {
+//                when(it.itemId) {
+//                    R.id.fetchFavorites -> {
+//                        true
+//                    }
+//                    R.id.fetchForGenre -> {
+//                        true
+//                    }
+//                    R.id.searchForName -> {
+//                        true
+//                    }
+//                    else -> {false}
+//                }
+//            }
+//        }
+//    }
